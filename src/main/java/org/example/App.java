572e29e0678d67b8493fbd3a4d4e04bf0be6172b
Daliya Benny <daliyaebenny@gmail.com>
2022-06-22 13:35:31 -0700

    Simple bean creation and DI

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,16 +11,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        //Tight coupling
-        System.out.println("Tight coupling with object creation");
-        Car car = new Car();
-        car.drive();
-        Bike bike = new Bike();
-        bike.drive();
-
-      // Created vehicle interface to reduce dependency
-        System.out.println("Created vehicle interface to reduce dependency");
-        Vehicle vehicle = new Bike();
+//        //Tight coupling
+//        System.out.println("Tight coupling with object creation");
+//        Car car = new Car();
+//        car.drive();
+//        Bike bike = new Bike();
+//        bike.drive();
+//
+//        // Created vehicle interface to reduce dependency
+//        System.out.println("Created vehicle interface to reduce dependency");
+//        Vehicle vehicle = new Bike();
+//        vehicle.drive();
+        //simple bean (non-parametrized) bean creation
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+        Vehicle vehicle = (Vehicle) context.getBean("Vehicle");
         vehicle.drive();
 
     }
